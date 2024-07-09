fb8567175de848e683192e971b70e44c4e1623e7
GoncharenkoVitaliy <Vitalesss.87@gmail.com>
2024-07-09 17:44:59 +0300

    подключение запроса модального ркна

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 "use strict"
 
-// import AppExtensionsSDK from './node_modules/@pipedrive/app-extensions-sdk';
+import AppExtensionsSDK from './node_modules/@pipedrive/app-extensions-sdk';
 
-// const sdk = await new AppExtensionsSDK().initialize();
-// // console.log('sdk:', sdk);
+const sdk = await new AppExtensionsSDK().initialize();
+// console.log('sdk:', sdk);
 
-// const { status } = await sdk.execute(AppExtensionsSDK.Command.OPEN_MODAL, {
-// 	type: AppExtensionsSDK.Modal.CUSTOM_MODAL,
-// 	action_id: 'Open settings',
-// 	data: {
-// 		item: 'xyz',
-// 	},
-// });
-// // console.log('status:', status);
+const { status } = await sdk.execute(AppExtensionsSDK.Command.OPEN_MODAL, {
+	type: AppExtensionsSDK.Modal.CUSTOM_MODAL,
+	action_id: 'Open settings',
+	data: {
+		item: 'xyz',
+	},
+});
+// console.log('status:', status);
 
 const form = document.querySelector('#form');
 form.addEventListener('submit', formSend);
